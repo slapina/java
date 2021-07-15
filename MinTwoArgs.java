a632c42671da1c48608efd692ecbd544e43b8e59
slapina <86593302+slapina@users.noreply.github.com>
2021-07-15 12:19:53 +0300

    Add files via upload

---
+++ MinTwoArgs.java
@@ -0,0 +1,13 @@
+public class MinTwoArgs {
+    public static void main(String[] args) {
+        int[] num = new int[] {10,20};
+        int min = num[0];
+        for (int i=0; i<num.length; i++){
+            if(num[i] < min){
+                min = num[i];
+            }
+        }
+        System.out.println(min);
+
+    }
+}
